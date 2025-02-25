24ae75fb7a2c43c978603f136c99b92641168ff2
NorseDevInc <147011498+NorseDevInc@users.noreply.github.com>
2025-02-25 01:08:29 -0500

    Removed incompatible code

--- BasicTemplate/src/main.cpp
+++ BasicTemplate/src/main.cpp
@@ -1,26 +1,6 @@
 #include <iostream>
-#include "raylib.h"
-#include "Ball.h"
 
 int main()
 {
-	Color backgroundColor = { 108, 153, 173, 255 };
-
-	InitWindow(800, 800, "My Template");
-	InitAudioDevice();
-	SetTargetFPS(60);
-
-	Ball ball;
-	
-
-	while (!WindowShouldClose())
-	{
-		ball.Move();
-
-		BeginDrawing();
-		ClearBackground(backgroundColor);
-		ball.Draw();
-		EndDrawing();
-	}
-	CloseWindow();
+	std::cout << "Hello world!" << std::endl;
 }
